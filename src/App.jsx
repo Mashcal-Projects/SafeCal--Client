481a6760da40b7b3e68328ab179b6a56934a0de5
Mashcal-Projects <mashcalprojects@gmail.com>
2025-01-14 09:00:07 +0200

    add nev import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ResetPasswordComp } from "./components/auth/ResetPassword";
 import { AuthProvider } from "./context/AuthContext";
 import { ReportProvider } from "./context/ReportContext";
 import { DashboardComp } from "./pages/Dashboard";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 
 function App() {
   return (
